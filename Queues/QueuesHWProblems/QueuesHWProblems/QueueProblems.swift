0af019428ec7982ea825b1be1914ad4d1a8f584f
Marcel324 <mchaucer324@gmail.com>
2016-11-16 14:21:46 -0500

    changes

--- Queues/QueuesHWProblems/QueuesHWProblems/QueueProblems.swift
+++ Queues/QueuesHWProblems/QueuesHWProblems/QueueProblems.swift
@@ -14,24 +14,62 @@ import Foundation
 
 //Find the sum of a queue
 func sum(q: Queue<Int>) -> Int? {
-    return nil
+    guard q.isEmpty() == false else {return nil}
+    
+    let replacementStack = Stack<Int>()
+    var sum = 0
+    while !q.isEmpty() {
+        let top = q.deQueue()!
+        sum += top
+        replacementStack.push(element: top)
+        }
+    while !replacementStack.isEmpty() {
+        q.enQueue(newElement: replacementStack.pop()!)
+    }
+    return sum
 }
 
 //Find the smallest element in a queue
 func smallest<T:Comparable>(q: Queue<T>) -> T? {
-    return nil
+    guard q.isEmpty() == false else {return nil}
+    var smallest = q.peek()
+    while !q.isEmpty() {
+       let top = q.deQueue()
+        if smallest! > top! {
+            smallest = top
+        }
+    }
+    return smallest
 }
 
 //Find out whether or not a queue is in sorted order from smallest to biggest
 //True example:   (Back) 9 - 6 - 2 - 1 (Front)
 //False example   (Back) 4 - 19 - 134 200 (Front)
-func isSorted<T: Comparable>(q: Queue<T>) -> Bool {
-    return false
+func isSorted<T: Comparable>(q: Queue<T>) -> Bool? {
+    guard q.isEmpty() == false else {return nil}
+    let smallest = q.peek()
+    while !q.isEmpty(){
+        let top = q.deQueue()
+        if smallest! > top! {
+            return false
+        }
+    }
+    return true
 }
 
 //Return a reversed queue.
 func reverse<T>(q: Queue<T>) -> Queue<T> {
-    return Queue<T>()
+    let replacementStack = Stack<T>()
+    let replacementQueue = Queue<T>()
+    while !q.isEmpty() {
+        let top = q.deQueue()
+        replacementStack.push(element: top!)
+    }
+    while !replacementStack.isEmpty() {
+        replacementQueue.enQueue(newElement: replacementStack.pop()!)
+    }
+    
+    return replacementQueue
 }
 
 //Determine if two queues are equal.
